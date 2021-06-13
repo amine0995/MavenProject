2c1dd3c466ed290a5a7285be471e895a3b51b26c
amine0995 <0995amina@gmail.com>
2021-06-13 15:22:54 +0200

    work 2

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -4,5 +4,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("developing new feature ");
+        System.out.println("local repository is already published to GitHup");
+        System.out.println(" come on, doing more work");
     }
 }
